19ea66fe45f5de6a0a6e6be7d2497706ca786727
Matthew Drumheller <matthew.drumheller@outlook.com>
2023-12-09 00:36:04 -0800

    Notes from Lesson 4

--- features/steps/target_search.py
+++ features/steps/target_search.py
@@ -7,19 +7,20 @@ def open_target(context):
     context.driver.get('https://www.target.com/')
 
 
-@when('Search for coffee')
-def search_product(context):
-    context.driver.find_element(By.ID, 'search').send_keys('coffee')
+@when('Search for {product}')
+def search_product(context, product):
+    context.driver.find_element(By.ID, 'search').send_keys(product)
     context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/Search/SearchButton']").click()
     sleep(6)  # wait for ads to disappear
 
 
-@then('Verify search worked')
-def verify_search(context):
+@then('Verify search worked for {search_result}')
+def verify_search(context, search_result):
     search_results_header = context.driver.find_element(By.CSS_SELECTOR, "[data-test='resultsHeading']").text
-    assert 'coffee' in search_results_header, f'Expected text coffee not in {search_results_header}'
+    assert search_result in search_results_header, f'Expected text {search_result} not in {search_results_header}'
 
 
-@then('Verify search result url')
-def verify_search_url(context):
-    assert 'coffee' in context.driver.current_url
+@then('Verify {expected_keyword} in search result url')
+def verify_search_url(context, expected_keyword):
+    assert expected_keyword in context.driver.current_url, \
+        f'Expected {expected_keyword} not in {context.driver.current_url}'
--- features/tests/target_search.feature
+++ features/tests/target_search.feature
@@ -2,8 +2,20 @@
 Feature: Search tests
 
 
-  Scenario: User can search for a product
+  Scenario: User can search for coffee
     Given Open target main page
     When Search for coffee
-    Then Verify search worked
-    And Verify search result url
+    Then Verify search worked for coffee
+    And Verify coffee in search result url
+
+  Scenario: User can search for a tea
+    Given Open target main page
+    When Search for tea
+    Then Verify search worked for tea
+    And Verify tea in search result url
+
+  Scenario: User can search for a mug
+    Given Open target main page
+    When Search for mug
+    Then Verify search worked for mug
+    And Verify mug in search result url
